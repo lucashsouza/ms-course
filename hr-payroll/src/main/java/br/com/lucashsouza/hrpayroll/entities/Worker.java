32466ff9e6ee00aca76a220c72c32816c6cd9318
Lucas Souza <lhenriquesouza00@gmail.com>
2021-09-30 23:45:40 -0300

    RestTemplate

---
+++ hr-payroll/src/main/java/br/com/lucashsouza/hrpayroll/entities/Worker.java
@@ -0,0 +1,62 @@
+package br.com.lucashsouza.hrpayroll.entities;
+
+import java.io.Serializable;
+
+public class Worker implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    private Long id;
+
+    private String name;
+
+    private double dailyIncome;
+
+    public Worker() {
+    }
+
+    public Worker(Long id, String name, double dailyIncome) {
+        this.id = id;
+        this.name = name;
+        this.dailyIncome = dailyIncome;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getDailyIncome() {
+        return dailyIncome;
+    }
+
+    public void setDailyIncome(double dailyIncome) {
+        this.dailyIncome = dailyIncome;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Worker worker = (Worker) o;
+
+        return id != null ? id.equals(worker.id) : worker.id == null;
+    }
+
+    @Override
+    public int hashCode() {
+        return id != null ? id.hashCode() : 0;
+    }
+}
